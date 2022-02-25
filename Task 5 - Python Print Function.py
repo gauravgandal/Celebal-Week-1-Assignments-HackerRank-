0d1e0f37a8b5b72e49031244ea775cc54c35ab8d
Gaurav14 <52061194+gauravgandal@users.noreply.github.com>
2022-02-25 21:37:56 +0530

    Add files via upload

---
+++ Task 5 - Python Print Function.py
@@ -0,0 +1,6 @@
+
+if __name__ == '__main__':
+    n = int(input())
+    i=1
+    for i in range(n):
+        print(str(i+1), end="")
---
+++ Task 63 - Python Input.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    x, k = map(int, input().strip().split())
+    string = input().strip()
+    
+    if eval(string) == k:
+        print(True)
+    else:
+        print(False)
